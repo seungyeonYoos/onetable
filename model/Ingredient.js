5d81d699adf3edae3b969301e3fc1bd545cb2409
GarlicScent <dongzai@naver.com>
2022-09-20 17:58:31 +0900

    four table setups

---
+++ model/Ingredient.js
@@ -0,0 +1,30 @@
+const Ingredient = (Sequelize, DataTypes) => {
+    const model = Sequelize.define(
+        "Ingredient", //테이블명
+        {
+            //테이블 정의 field를 적어둔다.
+            id: {
+                // sql문: id int not null primary key
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+                // comment: "hahahah"
+                // validate: 유효성 검사하는 속성
+            },
+            description: {
+                type: DataTypes.STRING(20),
+                allowNull: false,
+            },
+        },
+        {
+            // database 모델 정의 부분. mysql은 db 생성 시 적용함.
+            tableName: "Ingredient",
+            freezeTableName: true,
+            timestamps: false,
+        }
+    );
+    return model;
+};
+
+module.exports = Ingredient;
--- model/index.js
+++ model/index.js
@@ -17,6 +17,65 @@ db.User = require("./User")(sequelize, Sequelize);
 db.Recipe = require("./Recipe")(sequelize, Sequelize);
 db.UserWishList = require("./UserWishList")(sequelize, Sequelize);
 db.UserLikes = require("./UserLikes")(sequelize, Sequelize);
+db.Step = require("./Step")(sequelize, Sequelize);
+db.Ingredient = require("./Ingredient")(sequelize, Sequelize);
+db.Measurement = require("./Measurement")(sequelize, Sequelize);
+
+db.Recipe.hasMany(db.Step, {
+    foreignKey: "recipe_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
+});
+
+db.Step.belongsTo(db.Recipe, {
+    foreignKey: "recipe_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
+});
+
+db.Recipe.hasMany(db.RecipeIngredients, {
+    foreignKey: "recipe_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
+});
+
+db.RecipeIngredients.belongsTo(db.Recipe, {
+    foreignKey: "recipe_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
+});
+
+db.Ingredient.hasMany(db.RecipeIngredients, {
+    foreignKey: "ingredient_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
+});
+
+db.RecipeIngredients.belongsTo(db.Ingredient, {
+    foreignKey: "ingredient_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
+});
+
+db.Measurement.hasMany(db.RecipeIngredients, {
+    foreignKey: "measurement_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
+});
+
+db.RecipeIngredients.belongsTo(db.Measurement, {
+    foreignKey: "measurement_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
+});
 
 // <table 여러 개 일 때 primary key foreign key 연결>
 // db.Payment = require("../Payment")(sequelize, Sequelize);
