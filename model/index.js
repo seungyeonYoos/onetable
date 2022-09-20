559370915e01b1365d416a9f4e95bd2a6db9fbac
GarlicScent <dongzai@naver.com>
2022-09-20 18:12:55 +0900

    change file & table name

--- model/index.js
+++ model/index.js
@@ -3,10 +3,10 @@ const config = require("../config/config.json")["development"];
 
 const db = {};
 const sequelize = new Sequelize(
-  config.database, // 데이터베이스
-  config.username, // 사용자명
-  config.password, // 비밀번호
-  config // 정보 전체
+    config.database, // 데이터베이스
+    config.username, // 사용자명
+    config.password, // 비밀번호
+    config // 정보 전체
 );
 
 db.sequelize = sequelize;
@@ -23,6 +23,7 @@ db.Review = require("./Review")(sequelize, Sequelize);
 db.Step = require("./Step")(sequelize, Sequelize);
 db.Ingredient = require("./Ingredient")(sequelize, Sequelize);
 db.Measurement = require("./Measurement")(sequelize, Sequelize);
+db.RecipeIngredient = require("./RecipeIngredient")(sequelize, Sequelize);
 
 db.Recipe.hasMany(db.Step, {
     foreignKey: "recipe_id",
@@ -38,42 +39,42 @@ db.Step.belongsTo(db.Recipe, {
     onUpdate: "cascade",
 });
 
-db.Recipe.hasMany(db.RecipeIngredients, {
+db.Recipe.hasMany(db.RecipeIngredient, {
     foreignKey: "recipe_id",
     sourceKey: "id",
     onDelete: "cascade",
     onUpdate: "cascade",
 });
 
-db.RecipeIngredients.belongsTo(db.Recipe, {
+db.RecipeIngredient.belongsTo(db.Recipe, {
     foreignKey: "recipe_id",
     sourceKey: "id",
     onDelete: "cascade",
     onUpdate: "cascade",
 });
 
-db.Ingredient.hasMany(db.RecipeIngredients, {
+db.Ingredient.hasMany(db.RecipeIngredient, {
     foreignKey: "ingredient_id",
     sourceKey: "id",
     onDelete: "cascade",
     onUpdate: "cascade",
 });
 
-db.RecipeIngredients.belongsTo(db.Ingredient, {
+db.RecipeIngredient.belongsTo(db.Ingredient, {
     foreignKey: "ingredient_id",
     sourceKey: "id",
     onDelete: "cascade",
     onUpdate: "cascade",
 });
 
-db.Measurement.hasMany(db.RecipeIngredients, {
+db.Measurement.hasMany(db.RecipeIngredient, {
     foreignKey: "measurement_id",
     sourceKey: "id",
     onDelete: "cascade",
     onUpdate: "cascade",
 });
 
-db.RecipeIngredients.belongsTo(db.Measurement, {
+db.RecipeIngredient.belongsTo(db.Measurement, {
     foreignKey: "measurement_id",
     sourceKey: "id",
     onDelete: "cascade",
@@ -82,86 +83,86 @@ db.RecipeIngredients.belongsTo(db.Measurement, {
 
 // Category와 Recipe 관계
 db.Category.hasMany(db.Recipe, {
-  foreignKey: "category_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "category_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 db.Recipe.belongsTo(db.Category, {
-  foreignKey: "category_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "category_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 
 // Level와 Recipe 관계
 db.Level.hasMany(db.Recipe, {
-  foreignKey: "level_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "level_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 db.Recipe.belongsTo(db.Level, {
-  foreignKey: "level_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "level_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 
 // User와 Favorite
 db.User.hasMany(db.Favorite, {
-  foreignKey: "user_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "user_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 db.Favorite.belongsTo(db.User, {
-  foreignKey: "user_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "user_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 
 // Recipe와 Favorite
 db.Recipe.hasMany(db.Favorite, {
-  foreignKey: "recipe_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "recipe_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 db.Favorite.belongsTo(db.Recipe, {
-  foreignKey: "recipe_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "recipe_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 
 // User와 Review
 db.User.hasMany(db.Review, {
-  foreignKey: "user_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "user_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 db.Review.belongsTo(db.User, {
-  foreignKey: "user_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "user_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 
 // Recipe와 Review
 db.User.hasMany(db.Review, {
-  foreignKey: "recipe_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "recipe_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 db.Review.belongsTo(db.User, {
-  foreignKey: "recipe_id",
-  sourceKey: "id",
-  onDelete: "cascade",
-  onUpdate: "cascade",
+    foreignKey: "recipe_id",
+    sourceKey: "id",
+    onDelete: "cascade",
+    onUpdate: "cascade",
 });
 
 module.exports = db;
