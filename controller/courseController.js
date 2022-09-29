6bcda92de6c0441e8da50135c0119fc7a8e227e0
Jerry <miso6495@gmail.com>
2022-09-29 14:44:48 +0900

    course mainpage backend working

--- controller/courseController.js
+++ controller/courseController.js
@@ -20,88 +20,112 @@ const {
 // }
 
 //* course mainpage
-exports.main = async (req, res) => {
+// top3
+async function getBestCourses() {
+  const query = `SELECT 
+					  c.id,
+					  c.name,
+					  c.image,
+					  COUNT(*)
+					FROM course AS c INNER JOIN application AS a
+					ON c.id = a.course_id
+					WHERE c.date > CURDATE()
+					GROUP BY c.id, c.name, c.image
+					ORDER BY COUNT(*) DESC
+					LIMIT 3;`;
+  const bestCourses = await sequelize.query(query, { type: QueryTypes.SELECT });
+  return bestCourses;
+}
+// top 3 아래 부분
+async function getCourseDatas(query) {
   // query
   let query = "";
   // priceValue
   let priceValue;
-  if (req.query.price == "low") priceValue = " < 50000 ";
-  else if (req.query.price == "middle")
-    priceValue = " BETWEEN 50000 AND 80000 ";
+  if (query.price == "low") priceValue = " < 50000 ";
+  else if (query.price == "middle") priceValue = " BETWEEN 50000 AND 80000 ";
   else priceValue = " > 80000 ";
 
-  if (req.query.order && req.query.month && req.query.price) {
+  if (query.order && query.month && query.price) {
     query = `SELECT 
-                c.id,
-                c.name,
-                c.image,
-                c.price,
-                COUNT(*)
-              FROM course AS c INNER JOIN ${req.query.order} AS a
-              ON c.id = a.course_id
-              WHERE c.date > CURDATE()
-              AND MONTH(c.date) = '${req.query.month}'
-              AND price ${priceValue}
-              GROUP BY c.id, c.name, c.image, c.price
-              ORDER BY COUNT(*) DESC;`;
-  } else if (req.query.order && req.query.month) {
+              c.id,
+              c.name,
+              c.image,
+              c.price,
+              COUNT(*)
+            FROM course AS c INNER JOIN ${query.order} AS a
+            ON c.id = a.course_id
+            WHERE c.date > CURDATE()
+            AND MONTH(c.date) = '${query.month}'
+            AND price ${priceValue}
+            GROUP BY c.id, c.name, c.image, c.price
+            ORDER BY COUNT(*) DESC;`;
+  } else if (query.order && query.month) {
     query = `SELECT 
-                c.id,
-                c.name,
-                c.image,
-                c.price,
-                COUNT(*)
-              FROM course AS c INNER JOIN ${req.query.order} AS a
-              ON c.id = a.course_id
-              WHERE c.date > CURDATE()
-              AND MONTH(date) = '${req.query.month}'
-              GROUP BY c.id, c.name, c.image, c.price
-              ORDER BY COUNT(*) DESC;`;
-  } else if (req.query.order && req.query.price) {
+              c.id,
+              c.name,
+              c.image,
+              c.price,
+              COUNT(*)
+            FROM course AS c INNER JOIN ${query.order} AS a
+            ON c.id = a.course_id
+            WHERE c.date > CURDATE()
+            AND MONTH(date) = '${query.month}'
+            GROUP BY c.id, c.name, c.image, c.price
+            ORDER BY COUNT(*) DESC;`;
+  } else if (query.order && query.price) {
     query = `SELECT 
-                c.id,
-                c.name,
-                c.image,
-                c.price,
-                COUNT(*)
-              FROM course AS c INNER JOIN ${req.query.order} AS a
-              ON c.id = a.course_id
-              WHERE c.date > CURDATE()
-              AND price ${priceValue};
-              GROUP BY c.id, c.name, c.image, c.price
-              ORDER BY COUNT(*) DESC;`;
-  } else if (req.query.month && req.query.price) {
+              c.id,
+              c.name,
+              c.image,
+              c.price,
+              COUNT(*)
+            FROM course AS c INNER JOIN ${query.order} AS a
+            ON c.id = a.course_id
+            WHERE c.date > CURDATE()
+            AND price ${priceValue};
+            GROUP BY c.id, c.name, c.image, c.price
+            ORDER BY COUNT(*) DESC;`;
+  } else if (query.month && query.price) {
     query = `SELECT * FROM course
-              WHERE date > CURDATE()
-              AND MONTH(date) = '${req.query.month}'
-              AND price ${priceValue};`;
-  } else if (req.query.order) {
+            WHERE date > CURDATE()
+            AND MONTH(date) = '${query.month}'
+            AND price ${priceValue};`;
+  } else if (query.order) {
     query = `SELECT
-                c.id,
-                c.name,
-                c.image,
-                COUNT(*)
-              FROM course AS c INNER JOIN ${req.query.order} AS a
-              ON c.id = a.course_id
-              WHERE c.date > CURDATE()
-              GROUP BY c.id, c.name, c.image
-              ORDER BY COUNT(*) DESC;`;
-  } else if (req.query.month) {
+              c.id,
+              c.name,
+              c.image,
+              COUNT(*)
+            FROM course AS c INNER JOIN ${query.order} AS a
+            ON c.id = a.course_id
+            WHERE c.date > CURDATE()
+            GROUP BY c.id, c.name, c.image
+            ORDER BY COUNT(*) DESC;`;
+  } else if (query.month) {
     query = `SELECT * FROM course
-              WHERE date > CURDATE()
-              AND MONTH(date) = '${req.query.month}'`;
-  } else if (req.query.price) {
+            WHERE date > CURDATE()
+            AND MONTH(date) = '${query.month}'`;
+  } else if (query.price) {
     query = `SELECT * FROM course
-              WHERE date > CURDATE()
-              AND price ${priceValue};`;
+            WHERE date > CURDATE()
+            AND price ${priceValue};`;
   } else {
     query = `SELECT * FROM course
-              WHERE date > CURDATE()
-              ORDER BY id DESC;`;
+            WHERE date > CURDATE()
+            ORDER BY id DESC;`;
   }
-  let result = await sequelize.query(query, { type: QueryTypes.SELECT });
-  console.log("courseData:", result);
-  res.render("course", { courseData: result });
+  const courseDatas = await sequelize.query(query, { type: QueryTypes.SELECT });
+  return courseDatas;
+}
+
+exports.main = async (req, res) => {
+  const bestCourse = await getBestCourses();
+  console.log("bestCourse:", bestCourse);
+  const courseData = await getCourseDatas(req.query);
+  console.log("courseData:", courseData);
+
+  res.render("course", { bestCourse, courseData });
 };
 
 //* 등록부분
@@ -165,56 +189,6 @@ exports.course_apply = (req, res) => {
   Application.create(data1).then((result) => {
     console.log("course_apply:", result);
   });
-};
-
-exports.course_apply = async (req, res) => {
-  try {
-    const { imp_uid, merchant_uid } = req.body;
-
-    // 액세스 토큰(access token) 발급 받기
-    const getToken = await axios({
-      url: "https://api.iamport.kr/users/getToken",
-      method: "post", // POST method
-      headers: { "Content-Type": "application/json" }, // "Content-Type": "application/json"
-      data: {
-        imp_key: "4234662338446257", // REST API 키
-        imp_secret:
-          "qe4WA34s1r8kjnFZeKVGm0BcSvXzrht7OQDcIiZ6rWExsFv9H78mMdHICHYwBX2jsVXF8VmDVe1goyDM", // REST API Secret
-      },
-    });
-    const { access_token } = getToken.data.response; // 인증 토큰
-
-    // imp_uid로 아임포트 서버에서 결제 정보 조회
-    const getPaymentData = await axios({
-      url: `https://api.iamport.kr/payments/${imp_uid}`, // imp_uid 전달
-      method: "get", // GET method
-      headers: { Authorization: access_token }, // 인증 토큰 Authorization header에 추가
-    });
-    const paymentData = getPaymentData.data.response; // 조회한 결제 정보
-
-    // DB에서 결제되어야 하는 금액 조회
-    const order = await Orders.findById(paymentData.merchant_uid);
-    const amountToBePaid = order.amount; // 결제 되어야 하는 금액
-
-    // 결제 검증하기
-    // 결제 검증하기
-    const { amount, status } = paymentData;
-    if (amount === amountToBePaid) {
-      // 결제금액 일치. 결제 된 금액 === 결제 되어야 하는 금액
-      await Orders.findByIdAndUpdate(merchant_uid, { $set: paymentData }); // DB에 결제 정보 저장
-
-      switch (status) {
-        case "paid": // 결제 완료
-          res.send({ status: "success", message: "일반 결제 성공" });
-          break;
-      }
-    } else {
-      // 결제금액 불일치. 위/변조 된 결제
-      throw { status: "forgery", message: "위조된 결제시도" };
-    }
-  } catch (e) {
-    res.status(400).send(e);
-  }
 };
 
 //* 상세페이지
