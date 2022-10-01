9b01dd2f0f0a637b3cf735d2628b385599bc04a1
Jerry <miso6495@gmail.com>
2022-10-02 03:41:45 +0900

    searchPage css & MVC(SQL) complete + ejs working

--- controller/searchController.js
+++ controller/searchController.js
@@ -9,6 +9,32 @@ const {
   sequelize,
 } = require("../model");
 
-exports.main = (req, res) => {
+exports.main = async (req, res) => {
   res.render("search");
+};
+
+// 검색
+async function getCourses(searchWord) {
+  const query = `SELECT * FROM course 
+                WHERE name LIKE '%${searchWord}%';`;
+  const courses = await sequelize.query(query, {
+    type: QueryTypes.SELECT,
+  });
+  return courses;
+}
+async function getRecipes(searchWord) {
+  const query = `SELECT * FROM recipe
+                WHERE title LIKE '%${searchWord}%';`;
+  const recipes = await sequelize.query(query, {
+    type: QueryTypes.SELECT,
+  });
+  return recipes;
+}
+exports.search = async (req, res) => {
+  const courseData = await getCourses(req.query.search);
+  console.log("search_courseData:", courseData);
+  const recipeData = await getRecipes(req.query.search);
+  console.log("search_recipeData:", recipeData);
+  const searchWord = req.query.search;
+  res.render("search", { searchWord, courseData, recipeData });
 };
